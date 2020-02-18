4e1c609eafa1cf63981069ed6dfe661369347a52
suyuan <suyuan326@qq.com>
2020-02-18 20:44:37 +0800

    a single http server add content-type

--- http.demo.js
+++ http.demo.js
@@ -1,9 +1,28 @@
 const http = require('http');
 
-let count = 0;
 const server = http.createServer();
 server.on('request',(request,response)=>{
-  response.end(`you are the ${count} customer`);
-  count++;
+  const contentType = request.headers['content-type'];
+  switch (contentType) {
+    case 'text/plain':
+      response.setHeader('Content-Type','text/plain');
+      let str = '';
+      request.on('data',(data)=>{
+        str += data.toString('utf-8');
+      });
+      request.on('end',()=>{
+        response.end(`you send text/plain text: ${str}`)
+      });
+      break;
+    case  'application/json':
+      response.end('you send application/json');
+      break;
+    case 'multi-part/form-data':
+      response.end('you send form-data');
+      break;
+    default:
+      response.end('not supported content-type');
+      break;
+  }
 });
 server.listen(8989);
