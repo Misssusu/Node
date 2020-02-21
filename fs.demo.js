5a2377616cefc4c1af5e5cd6dea892d9289bfad6
suyuan <suyuan326@qq.com>
2020-02-21 12:19:13 +0800

    add fs 文件系统 读取文件

--- fs.demo.js
+++ fs.demo.js
@@ -3,6 +3,7 @@ const fstestobj = {
   name: 'suyuan',
   age: 18
 };
+let filePath = './fs.test.json';
 
 function fileExists(path,callback){
   fs.access(path,(error)=>{
@@ -13,10 +14,14 @@ function fileExists(path,callback){
     }
   })
 }
-fileExists('./fs.test.json',(error,fileexists)=>{
+fileExists(filePath,(error,fileexists)=>{
   if(!fileexists) {
-    fs.appendFile('./fs.test.json',JSON.stringify(fstestobj),(err)=>{
+    fs.appendFile(filePath,JSON.stringify(fstestobj),(err)=>{
       if (err) console.log(err);
+    });
+  }else {
+    fs.readFile(filePath,(error,data)=>{
+      console.log(data.toString('utf-8'));
     })
   }
 });
