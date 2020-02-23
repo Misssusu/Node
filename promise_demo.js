a308f6651b81727d31041a6ae28ab4fc67ab2e65
suyuan <suyuan326@qq.com>
2020-02-23 22:14:25 +0800

    add promise_demo.js

--- promise_demo.js
+++ promise_demo.js
@@ -1,7 +1,8 @@
 let promise = new Promise((resolve,reject)=>{
   console.log(1);  // 同步执行
-  resolve();  // pending -> resolved
+  resolve();  // pending -> resolved 状态一旦发生改变就不会再变
   reject();  // pending -> rejected
 });
 console.log(promise);  // pending
 console.log(2);
+//promise属于微任务
