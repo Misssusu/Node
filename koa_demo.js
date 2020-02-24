b28d070c3cbe03b898c08db315d9e915b8586a88
suyuan <suyuan326@qq.com>
2020-02-24 22:03:40 +0800

    add koa_demo.js 自定义一个中间件

--- koa_demo.js
+++ koa_demo.js
@@ -1,6 +1,11 @@
 const koa = require('koa');
 const app = new koa();
-app.use(async (context,next)=>{
-  context.body = 'hello';
-});
+function middleWare(options){
+  if(options.user){
+    return async (ctx,next)=>{ ctx.body='success' }
+  }else {
+    next();
+  }
+}
+app.use(middleWare({user: true}));
 app.listen(8989);
