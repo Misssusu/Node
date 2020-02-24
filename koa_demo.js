08c87b4ecce11a285e1e050322ee06132ec9d3a9
suyuan <suyuan326@qq.com>
2020-02-24 23:30:35 +0800

    add koa_demo.js use koa-router

--- koa_demo.js
+++ koa_demo.js
@@ -1,13 +1,22 @@
 const koa = require('koa');
 const bodyParse = require('koa-body');
+const Router = require('koa-router');
 const app = new koa();
 
+app.use(bodyParse());
+const router = new Router();
+let users = [];
+router
+  .get('/user',async (ctx,next)=>{
+    ctx.body = { data: { users } };
+  })
+  .post('/user',async (ctx,next)=>{
+    console.log(ctx.request.body);
+    const { user } = ctx.request.body;
+    users.push(user);
+    ctx.body = { data: { users } };
+  });
+app.use(router.routes());
 
-app.use(bodyParse({
-  json: false
-}));
-app.use(async (ctx,next)=>{
-  console.log(ctx.request.body);
-  ctx.body = 'got it';
-});
+
 app.listen(8989);
